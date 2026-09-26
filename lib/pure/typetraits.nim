c9486be96ca2bbb9b57ff527121d32cdc1919472
zerbina <100542850+zerbina@users.noreply.github.com>
2026-09-26 16:06:43 +0000

    typetraits: move magics to `system/magics`

--- lib/pure/typetraits.nim
+++ lib/pure/typetraits.nim
@@ -12,6 +12,7 @@
 ##
 ## Unstable API.
 
+import system/magics except error
 import std/private/since
 export system.`$` # for backward compatibility
 
@@ -30,81 +31,9 @@ runnableExamples:
   type C[T] = enum h0 = 2, h1 = 4
   assert C[float] is HoleyEnum
 
-proc name*(t: typedesc): string {.magic: "TypeTrait".} =
-  ## Returns the name of the given type.
-  ##
-  ## Alias for `system.\`$\`(t) <dollars.html#$,typedesc>`_ since Nim v0.20.
-  runnableExamples:
-    doAssert name(int) == "int"
-    doAssert name(seq[string]) == "seq[string]"
-
-proc arity*(t: typedesc): int {.magic: "TypeTrait".} =
-  ## Returns the arity of the given type. This is the number of "type"
-  ## components or the number of generic parameters a given type `t` has.
-  runnableExamples:
-    doAssert arity(int) == 0
-    doAssert arity(seq[string]) == 1
-    doAssert arity(array[3, int]) == 2
-    doAssert arity((int, int, float, string)) == 4
-
-proc genericHead*(t: typedesc): typedesc {.magic: "TypeTrait".} =
-  ## Accepts an instantiated generic type and returns its
-  ## uninstantiated form.
-  ## A compile-time error will be produced if the supplied type
-  ## is not generic.
-  ##
-  ## **See also:**
-  ## * `stripGenericParams proc <#stripGenericParams,typedesc>`_
-  runnableExamples:
-    type
-      Foo[T] = object
-      FooInst = Foo[int]
-      Foo2 = genericHead(FooInst)
-
-    doAssert Foo2 is Foo and Foo is Foo2
-    doAssert genericHead(Foo[seq[string]]) is Foo
-    doAssert not compiles(genericHead(int))
-
-    type Generic = concept f
-      type _ = genericHead(typeof(f))
-
-    proc bar(a: Generic): typeof(a) = a
-
-    doAssert bar(Foo[string].default) == Foo[string]()
-    doAssert not compiles bar(string.default)
-
-    when false: # these don't work yet
-      doAssert genericHead(Foo[int])[float] is Foo[float]
-      doAssert seq[int].genericHead is seq
-
-proc stripGenericParams*(t: typedesc): typedesc {.magic: "TypeTrait".} =
-  ## This trait is similar to `genericHead <#genericHead,typedesc>`_, but
-  ## instead of producing an error for non-generic types, it will just return
-  ## them unmodified.
-  runnableExamples:
-    type Foo[T] = object
-
-    doAssert stripGenericParams(Foo[string]) is Foo
-    doAssert stripGenericParams(int) is int
-
-proc supportsCopyMem*(t: typedesc): bool {.magic: "TypeTrait".}
-  ## This trait returns true if the type `t` is safe to use for
-  ## `copyMem`:idx:.
-  ##
-  ## Other languages name a type like these `blob`:idx:.
-
-proc supportsZeroMem*(t: typedesc): bool {.magic: "TypeTrait".}
-  ## This trait returns true if using `zeroMem`:idx: on a location of type `t`
-  ## brings the location into its "default-initialized" state. This doesn't
-  ## imply that using `zeroMem`:idx: on a location already storing a value is
-  ## valid.
-
-proc isNamedTuple*(T: typedesc): bool {.magic: "TypeTrait".} =
-  ## Returns true for named tuples, false for any other type.
-  runnableExamples:
-    doAssert not isNamedTuple(int)
-    doAssert not isNamedTuple((string, int))
-    doAssert isNamedTuple(tuple[name: string, age: int])
+export magics.name, magics.arity, magics.genericHead, magics.stripGenericParams
+export magics.supportsCopyMem, magics.supportsZeroMem
+export magics.isNamedTuple
 
 template pointerBase*[T](_: typedesc[ptr T | ref T]): typedesc =
   ## Returns `T` for `ref T | ptr T`.
@@ -116,19 +45,7 @@ template pointerBase*[T](_: typedesc[ptr T | ref T]): typedesc =
     assert (var s = "abc"; s[0].addr).typeof.pointerBase is char
   T
 
-proc distinctBase*(T: typedesc, recursive: static bool = true): typedesc {.magic: "TypeTrait".} =
-  ## Returns the base type for distinct types, or the type itself otherwise.
-  ## If `recursive` is false, only the immediate distinct base will be returned.
-  ##
-  ## **See also:**
-  ## * `distinctBase template <#distinctBase.t,T,static[bool]>`_
-  runnableExamples:
-    type MyInt = distinct int
-    type MyOtherInt = distinct MyInt
-    doAssert distinctBase(MyInt) is int
-    doAssert distinctBase(MyOtherInt) is int
-    doAssert distinctBase(MyOtherInt, false) is MyInt
-    doAssert distinctBase(int) is int
+export magics.distinctBase
 
 since (1, 1):
   template distinctBase*[T](a: T, recursive: static bool = true): untyped =
@@ -145,14 +62,7 @@ since (1, 1):
     else: # avoids hint ConvFromXtoItselfNotNeeded
       a
 
-  proc tupleLen*(T: typedesc[tuple]): int {.magic: "TypeTrait".} =
-    ## Returns the number of elements of the tuple type `T`.
-    ##
-    ## **See also:**
-    ## * `tupleLen template <#tupleLen.t>`_
-    runnableExamples:
-      doAssert tupleLen((int, int, float, string)) == 4
-      doAssert tupleLen(tuple[name: string, age: int]) == 2
+  export magics.tupleLen
 
   template tupleLen*(t: tuple): int =
     ## Returns the number of elements of the tuple `t`.
@@ -190,34 +100,7 @@ since (1, 3, 5):
 
     typeof(block: (for ai in a: ai))
 
-proc rangeBase*(t: typedesc): typedesc {.magic: "TypeTrait".} =
-  ## Returns the base type of the ``range`` type `t`. Only a single level is
-  ## skipped, that is, if the range type's base type is also a range type,
-  ## the base type is not skipped.
-  runnableExamples:
-    type
-      Range = range[1..3]
-      Nested = range[Range(1)..Range(3)]
-
-    doAssert rangeBase(Range) is int
-    doAssert rangeBase(Range) isnot range
-    doAssert rangeBase(Nested) is Range
-
-proc isCyclical*(t: typedesc): bool {.magic: "TypeTrait".} =
-  ## Returns whether the type `t` is *potentially* able to be part of a
-  ## reference cycle when used as the type of a managed heap location.
-  runnableExamples:
-    type
-      NoCycle = object
-        x: seq[NoCycle]
-      NoCycle2 {.acyclic.} = ref object
-        x: NoCycle2
-      Cycle = ref object
-        x: Cycle
-
-    doAssert not isCyclical(NoCycle)
-    doAssert not isCyclical(NoCycle2)
-    doAssert isCyclical(Cycle)
+export magics.rangeBase, magics.isCyclical
 
 import std/macros
 
